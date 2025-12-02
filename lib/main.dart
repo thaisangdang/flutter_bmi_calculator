2f96ce283a089c69a0bff4562739fd4626c6ea67
thaisangdang <thaisangdang@gmail.com>
2025-12-02 10:53:36 +0700

    Initial setup with dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
+import 'screens/input_page.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const BMICalculator());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      // THEME SETUP
+      // Instead of manual colors everywhere, we override the dark theme.
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: const Color(0xFF0A0E21),
+        scaffoldBackgroundColor: const Color(0xFF0A0E21), // Dark Navy
+        appBarTheme: const AppBarTheme(backgroundColor: Color(0xFF0A0E21), 
         ),
       ),
+      home: const InputPage(),
     );
   }
 }
